c43a5dfdac7a0fdda41d645f79624ac0ca60709c
sjdhome <sjdhome@foxmail.com>
2023-04-07 20:14:01 +0800

    Fix bugs according to the hints of IntelliJ IDEA

--- src/blog/js/index.js
+++ src/blog/js/index.js
@@ -1,12 +1,12 @@
-window.onSearch = (event) => {
-  const postList = document.getElementById("post-list");
-  const search = event.target.value.toLowerCase();
-  for (const post of postList.children) {
-    const title = post.getElementsByTagName("h2")[0].innerText.toLowerCase();
-    if (title.indexOf(search) > -1) {
-      post.style.display = "block";
-    } else {
-      post.style.display = "none";
+document.getElementById("search").addEventListener("input", (event) => {
+    const postList = document.getElementById("post-list");
+    const search = event.target.value.toLowerCase();
+    for (const post of postList.children) {
+        const title = post.getElementsByTagName("h2")[0].innerText.toLowerCase();
+        if (title.indexOf(search) > -1) {
+            post.style.display = "block";
+        } else {
+            post.style.display = "none";
+        }
     }
-  }
-};
+});
